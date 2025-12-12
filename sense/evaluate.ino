11e3b8384115b816081a3935e3bb6b4349e2b7e9
LEE LUNA <seojilee@student.42seoul.kr>
2025-12-12 19:48:35 +0900

    final - remove unnecessary comments

--- sense/evaluate.ino
+++ sense/evaluate.ino
@@ -6,8 +6,6 @@ void evaluate_risk(void) {
   float diff_gz = max_gz - min_gz;
   float diff_gx = max_gx - min_gx;
 
-  //Serial.println(String(diff_gx) + "\t" + String(diff_gz));
-
   float greater_g = diff_gz > diff_gx ? diff_gz : diff_gx;
   if (greater_g > 250 ||
         ((diff_az > 2.0 || diff_ax > 1.2) && greater_g > 200)) {
@@ -24,12 +22,3 @@ void evaluate_risk(void) {
   Serial.println("greater_g: " + String(greater_g));
   */
 }
-
-/*
- * 지금 할일:
- * 1. 블루투스 예준님 코드 이식하기.
- * 2. 블루투스 인식 안되면 아예 빼기.
- * 내일 할일:
- * 1. 스피커 앰프 납땜 다시. (수호)
- * 2. 접착력 강한 테이프 사기. (다이소)
- */
--- sense/sense.ino
+++ sense/sense.ino
@@ -44,12 +44,6 @@ void bleble();
 // 여기까지
 
 void setup() {
-  // 실제 시연에서는 뺀다.
-  /*
-  Serial.begin(9600);
-  while (!Serial);
-  */
-
   initialize_imu();
   initialize_mp3();
 
@@ -61,7 +55,6 @@ void loop() {
   if (millis() - scanned_time >= 60000) {
     bleble();
   }
-  // 현재 문제점: 블루투스 신호 잡히는 게 좀 늦어서 센서값 읽어오는 게 너무 느리다.
   static int idx = 0;
   sample_accel(idx);
   sample_gyro(idx);
@@ -77,13 +70,6 @@ void loop() {
     max_gz = max_arr(gyro[2], TP);
     min_gz = min_arr(gyro[2], TP);
 
-    //      Serial.print(200*(max_ax - min_ax));
-    //      Serial.print("\t");
-    //      Serial.print(200*(max_az - min_az));
-    //      Serial.print("\t");
-    //      Serial.print(max_gx - min_gx);
-    //      Serial.print("\t");
-    //      Serial.println(max_gz - min_gz);
     evaluate_risk();
     idx = 0;
   }
@@ -112,79 +98,17 @@ float min_arr(float *arr, int size) {
 void sample_accel(int idx) {
   while (!IMU.accelerationAvailable());
   IMU.readAcceleration(accel[0][idx], accel[1][idx], accel[2][idx]);
-  //Serial.println(String(accel[0][idx]) + "\t" + String(accel[2][idx]));
 }
 
 void sample_gyro(int idx) {
   while (!IMU.gyroscopeAvailable());
   IMU.readGyroscope(gyro[0][idx], gyro[1][idx], gyro[2][idx]);
-  //Serial.println(String(200) + "\t" + String(gyro[0][idx]) + "\t" + String(gyro[1][idx]) + "\t" + String(gyro[2][idx]) + "\t" + String(-200));
 }
 
 void bleble() { // 안전일때 0, 1/2단계일때 1/2반환
 
   BLEDevice peripheral;
-  // Serial.print("MAC: ");
-  // Serial.println(peripheral.address());
   String targetMac = "ec:62:60:80:fc:16";
   while (!(peripheral = BLE.available()) || (peripheral.address() != targetMac));
   scanned_time = millis();
-  /*
-     if (peripheral.localName() != "RP2040_Beacon") {
-     Serial.println("rp2040아님");
-     return;
-     }
-     else {
-     Serial.println("rp2040맞음");
-     }
-     */
-
-  /*
-     String mac = peripheral.address();
-     if (mac != targetMac) {
-  //Serial.println("Mac No");
-  return 1;
-  }
-  */
-
-  //int rssi = peripheral.rssi();
-  /* 우선 not filtered 보는중 *//*
-                                   filteredRSSI = (alpha * rssi) + ((1 - alpha) * filteredRSSI);
-
-                                   float n = 2.0;
-                                   distance = pow(10, (txPower - filteredRSSI) / (10 * n));*/
-  /*
-     float n = 2.0;
-     distance = pow(10, (txPower - rssi) / (10 * n));*/
-
-
-
-
-  /*
-     Serial.print("Device: ");
-     Serial.print(peripheral.localName());
-     Serial.print(" | Not filtered RSSI: ");
-     Serial.print(rssi);
-     */
-
-
-  /*
-     Serial.print(" | filtered RSSI: ");
-     Serial.print(filteredRSSI);
-     Serial.print(" dBm | filtered distance: ");
-     Serial.print(distance);
-     Serial.println(" m");
-
-     if (distance > 0 && distance <= danger_range) {
-     Serial.println("2단계 위험");
-     return 2;
-     }
-     else if (distance > danger_range && distance <= warning_range) {
-     Serial.println("1단계 경고");
-     return 1;
-     }
-     else {
-     Serial.println("안전 거리");
-     return 0;
-     }*/
 }
